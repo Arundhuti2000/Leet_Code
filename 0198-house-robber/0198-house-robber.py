045c6e190c3709502d7c594bb61e0d16b195ca68
Arundhati Das <44167900+Arundhuti2000@users.noreply.github.com>
2025-07-09 07:05:57 -0400

    Time: 0 ms (100%), Space: 17.9 MB (13.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,19 @@
 class Solution:
-    def robhouse(self,houses, i,dp):
-            if dp[i] is not None:
-                return dp[i]
-            if i <0:
+    def robhouse(self,houses, n,dp):
+            if n==0:
                 return 0
-            if i==0:
-                return houses[0]
-            if i==1:
-                return max(houses[0],houses[1])
-            ith_houose_stolen=houses[i] + self.robhouse(houses,i-2,dp)
-            ith_houose_not_stolen=self.robhouse(houses,i-1,dp)
-            dp[i]=max(ith_houose_stolen,ith_houose_not_stolen)
-            return dp[i]
+            dp[0]=houses[0]
+            for i in range(1,n):
+                if i==1:
+                    dp[1]=max(houses[0],houses[1])
+                else:
+                    ith_houose_stolen=houses[i] + dp[i-2]
+                    ith_houose_not_stolen=dp[i-1]
+                    dp[i]=max(ith_houose_stolen,ith_houose_not_stolen)
+            return dp[n-1]
     def rob(self, nums: List[int]) -> int:
         n=len(nums)
         dp = [None] * n
-        return self.robhouse(nums, n-1,dp)
+        return self.robhouse(nums, n,dp)
 
         
