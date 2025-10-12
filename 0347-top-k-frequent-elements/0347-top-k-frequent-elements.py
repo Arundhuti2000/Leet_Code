758529c930000638991e404805a4673b1b7ded9c
Arundhati Das <44167900+Arundhuti2000@users.noreply.github.com>
2025-10-12 13:47:13 -0400

    Time: 15 ms (10.14%), Space: 24.1 MB (20.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,15 @@
 class Solution:
-    def quicksort(self,arr: List[int], counts: dict):
-        if len(arr) <= 1:
-            return arr
-        pivot = arr[len(arr) // 2]
-        left = [x for x in arr if counts[x] > counts[pivot]]
-        middle = [x for x in arr if counts[x] == counts[pivot]]
-        right = [x for x in arr if counts[x] < counts[pivot]]
-        return self.quicksort(left, counts) + middle + self.quicksort(right, counts)
-
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts={}
+        freq=[[]for i in range(len(nums)+1)]
         result=[]
         i=0
         for num in nums:
             counts[num] = counts.get(num, 0) + 1
-        array=list(counts.keys())
-        sorted_array = self.quicksort(array,counts)
-        return sorted_array[:k]
+        for v, c in counts.items():
+            freq[c].append(v)
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result)==k:
+                    return result
